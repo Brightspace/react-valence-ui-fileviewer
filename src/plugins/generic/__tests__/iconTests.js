e075587c26bc21562d3dd3a79252846120d7bcea
Dave Lockhart <dave.lockhart@d2l.com>
2015-10-06 13:33:28 -0400

    tests for generic icon component

--- src/plugins/generic/__tests__/iconTests.js
+++ src/plugins/generic/__tests__/iconTests.js
@@ -4,10 +4,18 @@ jest.dontMock('../icon.js');
 
 var React = require('react/addons'),
 	TestUtils = React.addons.TestUtils,
-	Icon = require('../icon.js');
+	Icon = require('../icon.js'),
+	getIconClassName = require('../../../getIconClassName');
+
+getIconClassName.mockImpl(function() { return 'test'; });
 
 describe('Generic Icon View', function() {
-	it('should test things eventually', function() {
+
+	afterEach(function() {
+		getIconClassName.mockClear();
+	});
+
+	it('should render wrapper with expected class name', function() {
 		var elem = TestUtils.renderIntoDocument(
 			<Icon mimeType='image/jpeg' />
 		);
@@ -17,4 +25,24 @@ describe('Generic Icon View', function() {
 		);
 		expect(wrapper.length).toBe(1);
 	});
+
+	it('should pass mimeType to getIconClassName', function() {
+		TestUtils.renderIntoDocument(
+			<Icon mimeType='image/jpeg' />
+		);
+		expect(getIconClassName.mock.calls.length).toBe(1);
+		expect(getIconClassName.mock.calls[0][0]).toBe('image/jpeg');
+	});
+
+	it('should append mimeType-specific class name', function() {
+		var elem = TestUtils.renderIntoDocument(
+			<Icon mimeType='foo/bar' />
+		);
+		var wrapper = TestUtils.scryRenderedDOMComponentsWithClass(
+			elem,
+			'vui-fileviewer-icon-test'
+		);
+		expect(wrapper.length).toBe(1);
+	});
+
 });
