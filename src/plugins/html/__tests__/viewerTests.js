1e561f660d53b4a4bb0e8092f01d8c5c40863eb4
Dave Lockhart <dave.lockhart@d2l.com>
2015-10-06 15:42:10 -0400

    tests for html viewer

--- src/plugins/html/__tests__/viewerTests.js
+++ src/plugins/html/__tests__/viewerTests.js
@@ -1,24 +1,66 @@
 'use strict';
 
-jest.dontMock('../viewer.js');
+jest.autoMockOff();
 
 var React = require('react/addons'),
+	sinon = require('sinon'),
 	TestUtils = React.addons.TestUtils,
 	Viewer = require('../viewer.js');
 
 describe('HTML Viewer', function() {
-	it('should test things eventually', function() {
+
+	var addEventListener, removeEventListener;
+
+	beforeEach(function() {
+		addEventListener = sinon.spy(window, 'addEventListener');
+		removeEventListener = sinon.spy(window, 'removeEventListener');
+	});
+
+	afterEach(function() {
+		addEventListener.restore();
+		removeEventListener.restore();
+	});
+
+	it('should render wrapper with expected class name', function() {
 		var elem = TestUtils.renderIntoDocument(
 			<Viewer src='foo.bar' />
 		);
-		var wrapper = TestUtils.scryRenderedDOMComponentsWithClass(
+		var wrapper = TestUtils.findRenderedDOMComponentWithClass(
 			elem,
 			'vui-fileviewer-html-native'
 		);
-		expect(wrapper.length).toBe(1);
+		expect(wrapper).toBeDefined();
 	});
 
-	it('Calls the progressCallback and passes 100 in as the value', function() {
+	it('should render an IFRAME pointing at "src"', function() {
+		var elem = TestUtils.renderIntoDocument(
+			<Viewer src='foo.bar' />
+		);
+		var iframe = TestUtils.findRenderedDOMComponentWithTag(
+			elem,
+			'iframe'
+		);
+		expect(iframe).toBeDefined();
+		expect(React.findDOMNode(iframe).src).toBe('foo.bar');
+	});
+
+	it('should add event listener when mounted', function() {
+		TestUtils.renderIntoDocument(<Viewer src='foo.bar' />);
+		expect(addEventListener.calledOnce).toBeTruthy();
+	});
+
+	it('should remove event listener when unmounted', function() {
+		var elem = TestUtils.renderIntoDocument(
+			<Viewer src='foo.bar' />
+		);
+		var success = React.unmountComponentAtNode(
+			React.findDOMNode(elem).parentNode
+		);
+		expect(success).toBeTruthy();
+		expect(removeEventListener.calledOnce).toBeTruthy();
+	});
+
+	it('should calls the progressCallback and pass 100 in as the value', function() {
 
 		var progressFunc = jest.genMockFunction();
 
@@ -31,4 +73,5 @@ describe('HTML Viewer', function() {
 		expect(progressFunc.mock.calls.length).toBe(1);
 		expect(progressFunc.mock.calls[0][0]).toBe(100);
 	});
+
 });
