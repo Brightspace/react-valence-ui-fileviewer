00360f5478be3b806386a68ae7bf04d7f5e494bd
d2l-joelp <joel.poiron@d2l.com>
2015-11-25 15:25:50 -0600

    Use pdf.combined.js so that all PDFJS code is bundled with ours

--- src/plugins/pdf/pdfjs-lib.js
+++ src/plugins/pdf/pdfjs-lib.js
@@ -1,7 +1,6 @@
-/* global PDFJS */
 'use strict';
 
 // This module only exists to help unit testing the alternate viewer
 // since PDFJS is not very unit test friendly
-require('pdfjs-dist/build/pdf');
-PDFJS.workerSrc = 'pdf.worker.js';
+
+require('pdfjs-dist/build/pdf.combined');
