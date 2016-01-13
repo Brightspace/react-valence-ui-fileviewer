436bcd687903fee6045f17f6455251b6e764cbe3
Jeff Stefaniuk <Jeff.Stefaniuk@D2L.com>
2016-01-13 12:20:11 -0600

    better names for the tests

--- src/plugins/pdf/alternate/__tests__/pdfjsWorkerSrcInitTests.js
+++ src/plugins/pdf/alternate/__tests__/pdfjsWorkerSrcInitTests.js
@@ -37,21 +37,21 @@ describe('pdfjsWorkerSrcInit', function() {
 		xhr.restore();
 	});
 
-	describe('should get worker js file and set workerSrc to an object URL', function() {
+	describe('', function() {
 		beforeEach(function(done) {
 			var promise = pdfjsWorkerSrcInit();
 			requests[0].respond(200, { 'Content-Type': 'application/javascript'	}, 'var x = 10000;');
 			promise.then(done);
 		});
 
-		it('test', function() {
+		it('should get worker js file and set workerSrc to an object URL', function() {
 			expect(requests.length).toBe(1);
 			expect(pdfjs.workerSrc).toEqual(workerSrcObjectUrl);
 			expect(createObjectURL.calledOnce).toBe(true);
 		});
 	});
 
-	describe('should not get worker js file if it has already been retrieved', function() {
+	describe('', function() {
 		beforeEach(function(done) {
 			var promise1 = pdfjsWorkerSrcInit();
 			var promise2 = pdfjsWorkerSrcInit();
@@ -59,34 +59,34 @@ describe('pdfjsWorkerSrcInit', function() {
 			return Promise.all([promise1, promise2]).then(done);
 		});
 
-		it('test', function() {
+		it('should not get worker js file if it has already been retrieved', function() {
 			expect(requests.length).toBe(1);
 			expect(pdfjs.workerSrc).toEqual(workerSrcObjectUrl);
 			expect(createObjectURL.calledOnce).toBe(true);
 		});
 	});
 
-	describe('should not get worker js file if it has already been retrieved', function() {
+	describe('', function() {
 		beforeEach(function(done) {
 			var promise = pdfjsWorkerSrcInit();
 			requests[0].respond(404);
 			return promise.then(done);
 		});
 
-		it('test', function() {
+		it('should not get worker js file if it has already been retrieved', function() {
 			expect(requests.length).toBe(1);
 			expect(pdfjs.workerSrc).toEqual(initialWorkerSrcUrl);
 			expect(createObjectURL.called).toBe(false);
 		});
 	});
 
-	describe('should not get worker js file if it has already been retrieved', function() {
+	describe('', function() {
 		beforeEach(function(done) {
 			workerSrcIsCrossDomain = false;
 			return pdfjsWorkerSrcInit().then(done);
 		});
 
-		it('test', function() {
+		it('should not get worker js file if it has already been retrieved', function() {
 			expect(requests.length).toBe(0);
 			expect(pdfjs.workerSrc).toEqual(initialWorkerSrcUrl);
 			expect(createObjectURL.called).toBe(false);
